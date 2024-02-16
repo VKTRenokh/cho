e86713db6c9904b1efb0589e54a682d193a584fa
VKTRenokh <viktorenokh@gmail.com>
2024-02-16 11:49:05 +0200

    refactor: bigger image resolution

--- src/bot/commands/imagine/constants/width-height.ts
+++ src/bot/commands/imagine/constants/width-height.ts
@@ -1,5 +1,5 @@
-export const maxCanvasWidth = 2000
-export const maxCanvasHeight = 2000
+export const maxCanvasWidth = 4000
+export const maxCanvasHeight = 4000
 
 export const minCanvasWidth = 1
 export const minCanvasHeight = 1
